74143c7042321632a2cbaa15265621f6394ad590
Andrey Sokolov <sokolov-as-i@yandex.ru>
2016-10-26 01:58:27 +0300

    small intend correction

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 require('./middleware/configuration-check')( function() {
     var express = require('express'),
-    app = express(),
-    config = require('./middleware')(app),
-    http = require('http').Server(app);
+        app = express(),
+        config = require('./middleware')(app),
+        http = require('http').Server(app);
 
     http.listen(app.get('port'), function () {
         console.log('Server is up. Port: ' + app.get('port'));
